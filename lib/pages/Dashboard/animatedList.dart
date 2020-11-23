7fee234c31520111caabcfdc519d8c21a2474585
Priyanshu Agarwal <priyanshu.agarwal88@gmail.com>
2020-11-23 21:31:43 +0530

    animated list animation change

--- lib/pages/Dashboard/animatedList.dart
+++ lib/pages/Dashboard/animatedList.dart
@@ -11,7 +11,10 @@ final GlobalKey<AnimatedListState> listKey = GlobalKey();
 
 Widget buildRemovedItem(
     BuildContext context, int a, Animation<double> animation) {
-  return buildToBeRemovedItem(context, a, animation);
+  return BuildToBeRemovedItem(
+    animation: animation,
+    index: a,
+  );
 }
 
 class MyAnimatedList extends StatefulWidget {
@@ -48,16 +51,106 @@ class _MyAnimatedListState extends State<MyAnimatedList> {
 
 }
 
-buildToBeRemovedItem(
-  BuildContext context,
-  int index,
-  Animation<double> animation,
-) {
-  return BuildItem(
-    animation: animation,
-    context: context,
-    index: index,
-  );
+// buildToBeRemovedItem(
+// BuildContext context,
+// int index,
+// Animation<double> animation,
+// ) {
+//   return
+// }
+
+class BuildToBeRemovedItem extends StatefulWidget {
+  final int index;
+  final Animation<double> animation;
+  BuildToBeRemovedItem({this.index, this.animation});
+  @override
+  _BuildToBeRemovedItemState createState() => _BuildToBeRemovedItemState();
+}
+
+class _BuildToBeRemovedItemState extends State<BuildToBeRemovedItem>
+    with TickerProviderStateMixin {
+  Animation<Offset> offsetAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    offsetAnimation = Tween<Offset>(
+      begin: const Offset(1.5, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: widget.animation,
+      curve: Curves.easeOut,
+    ));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: offsetAnimation,
+      // alignment: Alignment.center,
+      // scale: widget.animation,
+      child: Card(
+          color: Colors.grey[900],
+          child: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Column(children: <Widget>[
+              (postDocumentSnapshots.length == 0)
+                  ? Container()
+                  : Post(doc: postDocumentSnapshots[widget.index]),
+              GestureDetector(
+                onTap: () {
+                  Alert(
+                      context: context,
+                      title: 'Delete this post?',
+                      style: AlertStyle(
+                        backgroundColor: Colors.black,
+                        titleStyle: GoogleFonts.ubuntu(color: Colors.white),
+                      ),
+                      buttons: [
+                        DialogButton(
+                            color: Colors.white,
+                            child: Text('Yes'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              firestore.deletePost(
+                                  postDocumentSnapshots[widget.index]);
+                              listKey.currentState.removeItem(
+                                widget.index,
+                                (BuildContext context,
+                                    Animation<double> animation) {
+                                  WidgetsBinding.instance
+                                      .addPostFrameCallback((_) {});
+                                  return buildRemovedItem(
+                                      context, widget.index, animation);
+                                },
+                                duration: const Duration(milliseconds: 800),
+                              );
+                            }),
+                        DialogButton(
+                            color: Colors.white,
+                            child: Text('No'),
+                            onPressed: null)
+                      ]).show();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                  child: Icon(
+                    Icons.delete,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ]),
+          )),
+    );
+  }
 }
 
 class BuildItem extends StatelessWidget {
@@ -71,56 +164,63 @@ class BuildItem extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Card(
-        color: Colors.grey[900],
-        child: Padding(
-          padding: const EdgeInsets.all(0.0),
-          child: Column(children: <Widget>[
-            Post(doc: postDocumentSnapshots[index]),
-            GestureDetector(
-              onTap: () {
-                Alert(
-                    context: context,
-                    title: 'Delete this post?',
-                    style: AlertStyle(
-                      backgroundColor: Colors.black,
-                      titleStyle: GoogleFonts.ubuntu(color: Colors.white),
-                    ),
-                    buttons: [
-                      DialogButton(
-                          color: Colors.white,
-                          child: Text('Yes'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                            firestore.deletePost(postDocumentSnapshots[index]);
-                            listKey.currentState.removeItem(
-                              index,
-                              (BuildContext context,
-                                  Animation<double> animation) {
-                                WidgetsBinding.instance
-                                    .addPostFrameCallback((_) {});
-                                return buildRemovedItem(
-                                    context, index, animation);
-                              },
-                              duration: const Duration(milliseconds: 500),
-                            );
-                          }),
-                      DialogButton(
-                          color: Colors.white,
-                          child: Text('No'),
-                          onPressed: null)
-                    ]).show();
-              },
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-                child: Icon(
-                  Icons.delete,
-                  size: 30,
-                  color: Colors.white,
+    return SizeTransition(
+      sizeFactor: animation,
+      axis: Axis.horizontal,
+      axisAlignment: -1.0,
+      child: Card(
+          color: Colors.grey[900],
+          child: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Column(children: <Widget>[
+              Post(doc: postDocumentSnapshots[index]),
+              GestureDetector(
+                onTap: () {
+                  Alert(
+                      context: context,
+                      title: 'Delete this post?',
+                      style: AlertStyle(
+                        backgroundColor: Colors.black,
+                        titleStyle: GoogleFonts.ubuntu(color: Colors.white),
+                      ),
+                      buttons: [
+                        DialogButton(
+                            color: Colors.white,
+                            child: Text('Yes'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              firestore
+                                  .deletePost(postDocumentSnapshots[index]);
+                              listKey.currentState.removeItem(
+                                index,
+                                (BuildContext context,
+                                    Animation<double> animation) {
+                                  WidgetsBinding.instance
+                                      .addPostFrameCallback((_) {});
+                                  return buildRemovedItem(
+                                      context, index, animation);
+                                },
+                                duration: const Duration(milliseconds: 900),
+                              );
+                              postDocumentSnapshots.removeAt(index);
+                            }),
+                        DialogButton(
+                            color: Colors.white,
+                            child: Text('No'),
+                            onPressed: null)
+                      ]).show();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                  child: Icon(
+                    Icons.delete,
+                    size: 30,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            )
-          ]),
-        ));
+              )
+            ]),
+          )),
+    );
   }
 }
